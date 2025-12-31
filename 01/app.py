9a7b49bee84c170492cda496dd0df59707e811ec
Ivan <ivan.s.bogush@gmail.com>
2025-12-31 23:03:01 +0300

    1

--- 01/app.py
+++ 01/app.py
@@ -1,1 +1,12 @@
+import pickle
+import streamlit as st
+
+@st.cache_resource  # Кэшируем модель (загружается только один раз)
+def load_model():
+    with open('models/churn_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+    with open('models/feature_names.pkl', 'rb') as f:
+        feature_names = pickle.load(f)
+    return model, feature_names
+
 
